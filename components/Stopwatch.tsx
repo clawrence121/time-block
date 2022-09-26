93d6183652ec6e6f38bca5d070a234a6300fd41b
Chris Lawrence <chris@resync.digital>
2022-09-26 18:11:36 +1000

    Updated timer logic to use data stamps for background sync

--- components/Stopwatch.tsx
+++ components/Stopwatch.tsx
@@ -11,21 +11,25 @@ import {
 export default function Stopwatch() {
   const [time, setTime] = useState(1800000);
   const [running, setRunning] = useState(false);
-  const [state, setState] = useState(0);
+  const [startTime, setStartTime] = useState<Date | null>(null);
 
   useEffect(() => {
     let interval: any;
 
     if (running) {
-      interval = setInterval(() => {
-        setTime((prevTime) => prevTime - 100);
-      }, 100);
+      if (startTime) {
+        interval = setInterval(() => {
+          setTime((prevTime) => startTime?.getTime() - Date.now());
+        }, 100);
+      } else {
+        setStartTime(new Date(new Date().getTime() + 1800000));
+      }
     } else if (!running) {
       clearInterval(interval);
     }
 
     return () => clearInterval(interval);
-  }, [running]);
+  }, [running, startTime]);
 
   return (
     <div className="flex flex-col relative min-h-[35rem] justify-between pt-[5vh] max-w-3xl mx-auto">
@@ -41,7 +45,7 @@ export default function Stopwatch() {
         <h2 className="text-3xl text-gray-50 font-semibold flex gap-4">
           I would like to block 30 minutes to focus on:
         </h2>
-        <div className="border-b border-gray-200 focus-within:border-indigo-600">
+        <div className="border-b-2 border-gray-200 focus-within:border-pink-400 transition duration-150">
           <label htmlFor="comment" className="sr-only">
             What do you need to focus on?
           </label>
@@ -49,7 +53,7 @@ export default function Stopwatch() {
             rows={3}
             name="comment"
             id="comment"
-            className="block w-full resize-none placeholder-gray-300 text-gray-100 p-0 pb-2 bg-purple-700 focus:outline-none"
+            className="block w-full resize-none placeholder-gray-200 text-gray-100  bg-purple-700 focus:outline-none"
             placeholder="What do you need to focus on?"
             defaultValue={""}
             disabled={running}
@@ -57,33 +61,27 @@ export default function Stopwatch() {
         </div>
         <div className="flex justify-between items-end">
           <button
-            className="flex items-center group text-4xl text-gray-50"
+            className="flex items-center group text-5xl text-gray-50"
             onClick={() => setRunning(true)}
           >
             Start
             <ArrowRightCircleIcon className="group-hover:translate-x-2 opacity-0 group-hover:opacity-100 transition duration-150 h-12" />
           </button>
-          <div className="space-x-2 text-white text-xl">
+          <div className="text-white text-md items-end flex flex-col">
             <button onClick={() => setRunning(false)}>Stop</button>
-            <button onClick={() => setTime(1800000)}>Reset</button>
+            <button
+              onClick={() => {
+                setTime(1800000);
+                setStartTime(null);
+                setRunning(false);
+              }}
+              className="text-purple-200 hover:text-purple-400 transition duration-150"
+            >
+              Reset
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
-}
-
-{
-  /* <div className="stopwatch">
-      <div className="numbers">
-        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
-        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
-      </div>
-      <div className="buttons">
-        <button onClick={() => setRunning(true)}>Start</button>
-        <button onClick={() => setRunning(false)}>Stop</button>
-        <button onClick={() => setTime(0)}>Reset</button>
-      </div>
-    </div> */
 }
