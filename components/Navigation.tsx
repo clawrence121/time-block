06aed968045b21f60a38043bbc67d23d228858c2
Chris Lawrence <chris@resync.digital>
2022-09-26 17:40:24 +1000

    Updated url

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -14,17 +14,6 @@ export default function Navigation() {
         <>
           <div className="mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white sm:hidden">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
                   <span className="text-gray-50 font-semibold text-2xl">
@@ -66,7 +55,7 @@ export default function Navigation() {
                           <a
                             target="_blank"
                             rel="noopener noreferrer"
-                            href="https://twitter.com/intent/tweet?text=I'm using TimeBlock to help block my time!&url=https://timeblock.vercel.app"
+                            href="https://twitter.com/intent/tweet?text=I'm using TimeBlock to help block my time!&url=https://time-block.app"
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700"
